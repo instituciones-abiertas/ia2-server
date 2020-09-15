afbaae19be9b1f57e647b7f7dda4db3861809f27
Charlie <charlie@camba.coop>
2020-09-15 18:16:07 -0300

    add spacy dependencies ,change applicationname and upgrade Readme

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applicationname.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liberajus-api-backend.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
