549ee5b3015793758ec3231ed9d234a7d9dafb70
Charlie <charlie@camba.coop>
2020-09-29 15:11:54 -0300

    modificado referencia de nombre applicationname

--- liberajus-api-backend/wsgi.py
+++ liberajus-api-backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applicationname.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liberajus-api-backend.settings.production')
 
 application = get_wsgi_application()
