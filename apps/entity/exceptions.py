ae1320ab2ccbf1460c9afe8f5c3ef5bbac3d881e
Charlie <charlie@camba.coop>
2020-11-10 17:30:19 -0300

    excepcion credencial de dropbox

--- apps/entity/exceptions.py
+++ apps/entity/exceptions.py
@@ -15,3 +15,8 @@ class ActFileNotFound(APIException):
     status_code = 404
     default_detail = settings.ERROR_ACT_FILE_NOT_FOUND
     default_code = 'act_file_not_found'
+
+class DropboxExpireCredentials(APIException):
+    status_code = 401
+    default_detail = settings.ERROR_DROPBOX_CREDENTIALS
+    default_code = 'dropbox_credentials'
