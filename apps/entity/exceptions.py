c75f34320627f53ac2e6a4f5766b1f7a5c072838
Carlos Cuoco <charlie@camba.coop>
2020-11-11 18:51:25 +0000

    agregado chequeo de codigo con flake8

--- apps/entity/exceptions.py
+++ apps/entity/exceptions.py
@@ -1,22 +1,32 @@
 from rest_framework.exceptions import APIException
 from django.conf import settings
 
+
 class ServiceOddDocumentUnavailable(APIException):
     status_code = 503
     default_detail = settings.ERROR_OODOCUMENT_NOT_WORKING
-    default_code = 'service_odddocument_unavailable'
+    default_code = "service_odddocument_unavailable"
+
 
 class nameTooLong(APIException):
     status_code = 400
     default_detail = settings.ERROR_NAME_TOO_LONG
-    default_code = 'service_document'
+    default_code = "service_document"
+
 
 class ActFileNotFound(APIException):
     status_code = 404
     default_detail = settings.ERROR_ACT_FILE_NOT_FOUND
-    default_code = 'act_file_not_found'
+    default_code = "act_file_not_found"
+
 
 class DropboxExpireCredentials(APIException):
     status_code = 401
     default_detail = settings.ERROR_DROPBOX_CREDENTIALS
-    default_code = 'dropbox_credentials'
+    default_code = "dropbox_credentials"
+
+
+class DriveExpireCredentials(APIException):
+    status_code = 401
+    default_detail = settings.ERROR_DRIVE_CREDENTIALS
+    default_code = "drive_credentials"
