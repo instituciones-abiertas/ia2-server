fe72ff004456244701e228f7585134f4115697dd
José Luis Di Biase <josx@interorganic.com.ar>
2020-11-27 18:11:22 -0300

    Generalizando llamadas a extraer_datos_de_ocurrencias con diversas entidades
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- apps/entity/utils/general.py
+++ apps/entity/utils/general.py
@@ -2,6 +2,14 @@ from ..models import Entity, Act, OcurrencyEntity, LearningModel
 from ..exceptions import ActNotExist, StorageFileNotExist
 from apps.data.helpers import extraer_datos
 
+# Esta dependencia esta solamente en produccion
+try:
+    from sentry_sdk import capture_exception
+except ImportError:
+
+    def capture_exception(err):
+        print(err)
+
 
 def check_exist_act(pk):
     try:
@@ -34,15 +42,17 @@ def calculate_ents_anonimyzed(arrayEnts):
     return result_list
 
 
-def buscar_entidad(ocurrencias, entidad):
-    return next((o.text for o in ocurrencias if o.entity == entidad), None)
-
-
 def extraer_datos_de_ocurrencias(ocurrencias):
     # Consideracion: solo se queda con la 1er ocurrencia de estas entiedades
-    lugar = buscar_entidad(ocurrencias, "LUGAR_HECHO")
-    fecha = buscar_entidad(ocurrencias, "FECHA_HECHO")
-    contexto_violencia = buscar_entidad(ocurrencias, "CONTEXTO_VIOLENCIA")
-    contexto_violencia_de_genero = buscar_entidad(ocurrencias, "CONTEXTO_VIOLENCIA_DE_GENERO")
-    if lugar or fecha or contexto_violencia or contexto_violencia_de_genero:
-        extraer_datos(contexto_violencia, contexto_violencia_de_genero, lugar, fecha)
+
+    def buscar(entidad):
+        return next((o.text for o in ocurrencias if o.entity == entidad), None)
+
+    entidades = ["CONTEXTO_VIOLENCIA", "CONTEXTO_VIOLENCIA_DE_GENERO", "LUGAR_HECHO", "FECHA_HECHO"]
+    contexto_violencia, contexto_violencia_de_genero, lugar, fecha = list(map(buscar, entidades))
+
+    if contexto_violencia or contexto_violencia_de_genero or lugar or fecha:
+        try:
+            extraer_datos(contexto_violencia, contexto_violencia_de_genero, lugar, fecha)
+        except Exception as error:
+            capture_exception(error)
