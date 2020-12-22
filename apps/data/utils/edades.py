a1b33a3a4ef04a51d39619bb8c4de430e03d0f8b
Charlie <charlie@camba.coop>
2020-12-22 20:25:07 -0300

    modificacion a la logica de la asignacion de la franja
    Former-commit-id: 2d0c7429d9e2ca2884113f34e112c0a8c603c2d2

--- apps/data/utils/edades.py
+++ apps/data/utils/edades.py
@@ -1,16 +1,19 @@
 def get_franja(numero):
-    # Diferencia si no se etiqueto
+    # Chequea  si no esta  etiqueto
     if numero != None:
         try:
             numero_escrito = int(numero)
             decena = numero_escrito // 10
-            if decena > 0:
+            resto = numero_escrito % 10
+            if decena > 0 and resto > 0:
                 return (decena + 1) * 10
+            elif decena > 0 and resto == 0:
+                return (decena) * 10
             else:
                 return 10
         except Exception as e:
             # En caso que se etiquete algo que no sea un numero
-            print("No se etiqueto un numero, se etiqueto :", numero)
+            print("No se etiqueto una edad, se etiqueto :", numero)
         else:
             return None
     else:
