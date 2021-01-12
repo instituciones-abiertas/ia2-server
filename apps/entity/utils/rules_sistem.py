b331fb91cbf60fa8ae150c77d8054ae2fdc02eec
Jose Luis Di Biase <josx@camba.coop>
2021-01-12 13:07:54 +0000

    Agregando reglas patrones para reconocimento de entidades: pasaporte, link, actuacion, expediente, archivo, usuarix, CBU, banco, patente, marca automovil
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>
    
    Former-commit-id: a8b451b43b33fb6a52aaa5b059b8564fd7b11f17

--- apps/entity/utils/rules_sistem.py
+++ apps/entity/utils/rules_sistem.py
@@ -2,16 +2,19 @@ from .general import filter_spans
 from .matcher import use_matchers
 from .custom_rules import use_rules
 
+
 def add_rules_sistem(nlp, doc, list_ents, matcher):
-    # Usamos los matchers creados y obtengo ocurrencias
-    matcher_ents = use_matchers(nlp, matcher, doc)
-    # Filtramos ocurrencias encontradas mirando si ya estan reconocidas como entidades
-    filtered_matcher_ents = filter_spans(matcher_ents, list_ents)
-    # Agrego a la lista de entidades las ocurrencias resultantes
-    list_ents = list_ents + (filtered_matcher_ents)
+    if matcher:
+        # Usamos los matchers creados y obtengo ocurrencias
+        matcher_ents = use_matchers(nlp, matcher, doc)
+        # Filtramos ocurrencias encontradas mirando si ya estan reconocidas como entidades
+        filtered_matcher_ents = filter_spans(matcher_ents, list_ents)
+        # Agrego a la lista de entidades las ocurrencias resultantes
+        list_ents = list_ents + (filtered_matcher_ents)
+
     # Usamos las reglas creadas y obtengo ocurrencias
     ents_by_rules = use_rules(doc, list_ents)
     # Filtramos ocurrencias encontradas mirando si ya estan reconocidas como entidades
-    filter_ents =  filter_spans(list_ents, ents_by_rules)
-    list_ents = ents_by_rules + filter_ents    
+    filter_ents = filter_spans(list_ents, ents_by_rules)
+    list_ents = ents_by_rules + filter_ents
     return list_ents
