2469abeac7595c99da1caba86876424a40717013
Santiago Botta <sgobotta@gmail.com>
2021-01-27 14:02:32 -0300

    :wrench: Temporarily removes the worker service
    Former-commit-id: fb8e66877a9496cbda6429fdb7b648279f4be0ae

--- liberajus/__init__.py
+++ liberajus/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
+# Comments the Celery instance until we develop re-training
+
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+# from .celery import app as celery_app
 
-__all__ = ['celery_app']
+# __all__ = ['celery_app']
