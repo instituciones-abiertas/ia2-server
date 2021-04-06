ab9579daee736a8b83e5adb3cce272df4c7277a9
Charlie <charlie@camba.coop>
2021-04-06 11:37:58 -0300

    agregado visualización en lista en Admin para ActStats

--- apps/entity/admin.py
+++ apps/entity/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Entity, Act, OcurrencyEntity
+from datetime import timedelta
+from .models import Entity, Act, OcurrencyEntity, ActStats
 
 admin.site.register(Entity)
 admin.site.register(Act)
 admin.site.register(OcurrencyEntity)
+
+
+class ActStatsAdmin(admin.ModelAdmin):
+    list_display = (
+        "act_id",
+        "load_time_seconds",
+        "detection_time_seconds",
+        "anonymization_time_seconds",
+        "extraction_time_seconds",
+    )
+
+
+admin.site.register(ActStats, ActStatsAdmin)
