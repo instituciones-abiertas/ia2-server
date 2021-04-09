24351c0c8cc0ec58d247fa65721d8ed5d8d532b7
Santiago <santiago@camba.coop>
2021-04-09 11:45:24 -0300

    :computer: Adds an Act factory

--- apps/entity/tests/factories.py
+++ apps/entity/tests/factories.py
@@ -1,13 +1,23 @@
 import factory
-from apps.entity.models import Entity
+from django.core.files.uploadedfile import SimpleUploadedFile
+from apps.entity.models import Act, Entity
 
 
 class EntityFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Entity
+
     name = "an entity"
     description = "this entity represents something in a text"
     should_anonimyzation = False
     should_trained = False
     enable_multiple_selection = False
 
+
+class ActFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = Entity
+        model = Act
+
+    text = "a text"
+    file = SimpleUploadedFile("a-file.docx", b"file contents.")
+    offset_header = 0
