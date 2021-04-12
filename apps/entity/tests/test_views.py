e715a1036a45ac208f82b97173f6274be11fd54b
Santiago <santiago@camba.coop>
2021-04-12 15:41:37 -0300

    :test_tube: Updates entity view tests with user test support utils

--- apps/entity/tests/test_views.py
+++ apps/entity/tests/test_views.py
@@ -1,5 +1,5 @@
 from apps.accounts.models import User
-from apps.accounts.factories import UserFactory
+from apps.accounts.tests.support import create_and_login_user
 from apps.entity.tests.factories import EntityFactory
 from django.contrib.auth import authenticate, login
 from django.test import Client, TestCase, tag
@@ -9,22 +9,12 @@ from rest_framework.test import APITestCase
 
 class EntityViewSetTest(APITestCase):
     def setUp(self):
-        self.entities = EntityFactory.create_batch(10)
-
-        user = UserFactory.create()
-        self.user_password = "examplepassword"
-        user.set_password(self.user_password)
-        user.save()
-        self.assertTrue(user.check_password(self.user_password))
-
-        self.user = user
         self.client = Client()
+        self.entities = EntityFactory.create_batch(10)
+        create_and_login_user(self.client)
 
-    @tag("wip")
     def test_an_authenticated_user_gets_an_entity_list(self):
-        self.client.login(username=self.user.get_username(), password=self.user_password)
         url = reverse("entity-list")
         response = self.client.get(url)
-
         self.assertEquals(response.status_code, 200)
         self.assertEquals(len(response.data), len(self.entities))
