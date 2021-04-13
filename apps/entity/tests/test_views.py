3f9ee57144ba0e90007de0377d660e7cdb801f50
Santiago <santiago@camba.coop>
2021-04-13 12:48:21 -0300

    :test_tube: Refactors entity viewset tests to use fixtures

--- apps/entity/tests/test_views.py
+++ apps/entity/tests/test_views.py
@@ -1,6 +1,7 @@
 from apps.accounts.models import User
 from apps.accounts.tests.support import create_and_login_user
-from apps.entity.tests.factories import EntityFactory
+from apps.entity.models import Entity
+from apps.entity.tests.factories import ActFactory, EntityFactory
 from django.contrib.auth import authenticate, login
 from django.test import Client, TestCase, tag
 from django.urls import reverse
@@ -8,16 +9,18 @@ from rest_framework.test import APITestCase
 
 
 class EntityViewSetTest(APITestCase):
+    fixtures = ["1_entity.json"]
+
     def setUp(self):
         self.client = Client()
-        self.entities = EntityFactory.create_batch(10)
 
     def test_an_authenticated_user_gets_an_entity_list(self):
         create_and_login_user(self.client)
         url = reverse("entity-list")
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(response.data), len(self.entities))
+        entities = list(Entity.objects.all())
+        self.assertEquals(len(response.data), len(entities))
 
     def test_an_unauthenticated_user_cannot_get_an_entity_list(self):
         url = reverse("entity-list")
