74915e8273832e4dd105e13620ed91ece7a741a9
martin.vallone <martin.vallone@fiqus.com>
2021-06-04 19:14:02 -0300

    Se extrae la generacion del doc fuera de la iteracion, se refactorea para mejorar la legibilidad del codigo y extraccion de funciones mas atomicas

--- apps/entity/utils/vistas.py
+++ apps/entity/utils/vistas.py
@@ -132,9 +132,8 @@ def overlap_ocurrency_list(ent_start, ent_end, original_ocurrency_list):
     return any(overlap_ocurrency(ent_start, ent_end, ocurrency) for ocurrency in original_ocurrency_list)
 
 
-def find_all_spans_of_ocurrency(text, ent, original_ent_list):
-    nlp = Nlp()
-    doc = nlp.generate_doc(text)
+def find_all_spans_of_ocurrency(text, doc, ent, original_ent_list):
+    print("se llamó a find_all_spans_of_ocurrency")
     ent_text = doc.char_span(ent.startIndex, ent.endIndex).text
     # en el doc busco las nuevas entidades que matcheen con el texto ent_text
     # filtrando aquellas que overlapeen con las entidades originales
@@ -142,15 +141,21 @@ def find_all_spans_of_ocurrency(text, ent, original_ent_list):
     for match in finditer(ent_text, text):
         if not overlap_ocurrency_list(match.span()[0], match.span()[1], original_ent_list):
             new_span = doc.char_span(match.span()[0], match.span()[1], ent.entity.name)
-            result.append(new_span)
+            if new_span:
+                result.append(new_span)
     return result
 
 
-def find_all_ocurrencies(text, original_ocurrencies, tag_list):
+def find_all_ocurrencies(text, doc, original_ocurrencies, tag_list):
+    # FIXME se puede usar asyncio? deberíamos extraer la función que obtiene ocurrencias de un tag
+
     # Filtro las ocurrencias cuyo nombre de tag coincide con los de las entidades sobre las que aplicar la funcionalidad de múltiple selección
     filtered_ocurrencies = list(filter(lambda x: (x.entity.id in tag_list), original_ocurrencies))
     new_ocurrencies = list(
-        map(lambda ocurrency: find_all_spans_of_ocurrency(text, ocurrency, original_ocurrencies), filtered_ocurrencies)
+        map(
+            lambda ocurrency: find_all_spans_of_ocurrency(text, doc, ocurrency, original_ocurrencies),
+            filtered_ocurrencies,
+        )
     )
     result = filter_spans([ent for ent_list in new_ocurrencies for ent in ent_list])
     return format_spans(result)
